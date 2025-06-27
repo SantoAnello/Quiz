20a2597cf91ae5d1f2eca6fc287d0fb13c9c2ac2
SantoAnello <mpsantiagoo@gmail.com>
2025-06-27 11:15:16 -0300

    Add files via upload
    Corrige export default no proxy.js para Netlify

---
+++ proxy.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch';
+
+export default async (event, context) => {
+  try {
+    const body = JSON.parse(event.body);
+
+    const googleResponse = await fetch(
+      "https://script.google.com/macros/s/AKfycbwauTT2Z-7k00JDjb4q6_ZR0q4fGGMW3cNTWcKw_zTT3ecYgC8xWso8AebH-kPjjCgX/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      }
+    );
+
+    const data = await googleResponse.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    };
+
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
